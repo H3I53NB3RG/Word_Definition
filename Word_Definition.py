6e1f771a0d2d6c54fc35938657abc01ed0f4f347
H3I53NB3RG <58789050+H3I53NB3RG@users.noreply.github.com>
2020-04-22 05:47:51 -0700

    Add files via upload

---
+++ Word_Definition.py
@@ -0,0 +1,31 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        answer = input("[-] Do u mean " + get_close_matches(w,data.keys())[0] + " ? Enter Y if yes, or N if no : ").upper()
+        if answer == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif answer == "N":
+            return "The Word Dosen't Exsist !"
+        else:
+            return "[-] Sorry, But I Don't Understand What The Fuck Do U Want :("
+    else:
+        return "The Word Dosen't Exsist !"
+
+word = input("[+] Enter a Word : ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print("- " + item)
+else:
+    print("[-] " + output)
